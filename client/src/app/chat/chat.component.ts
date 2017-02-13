92a99a7848eaa23493716bd0eea2599b8b1c8f89
lkuta <kutalukasz@gmail.com>
2017-02-13 22:44:18 +0100

    Revert "Revert "fixes and add some small features""
    This reverts commit 8fe20fed0ea619461595fc91dbf978cf23939d44.

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -15,7 +15,7 @@ export class ChatComponent implements OnInit, OnDestroy{
   connectionMsgs;
   connectionUsers;
   message = '';
-  username;
+  username = '';
   users = [];
 
   title = 'ChatApp';
@@ -26,30 +26,54 @@ export class ChatComponent implements OnInit, OnDestroy{
 
   sendMessage(){
     if(/\S/.test(this.message)){
-      this.chatService.sendMessage(this.username + ': ' + this.message);
+      this.chatService.sendMessage(this.message, this.username);
       this.message = '';
     }
   }
 
-  registerUser(){
-    console.log('registerUser() fired: hello ' + this.username);
-    this.ref.nativeElement.querySelector('#login-box').remove();
-    this.ref.nativeElement.querySelector('#message-form > input').focus();
-    this.chatService.initSocket();
-    this.chatService.sendUsername(this.username);
+  putMessage(message){
+    if(message.username == this.username){
+      return message.text;
+    } else {
+      return message.username + ': ' + message.text;
+    }
+  }
 
-    this.connectionMsgs = this.chatService.getMessages().subscribe(message => {
-      this.messages.push(message);
-      this.updateScroll();
-    });
-    this.connectionUsers = this.chatService.getUsers().subscribe(user => {
-      this.users = user as string[];
-    });
+  registerUser(){
+    if(!this.loginValidation()){
+      this.ref.nativeElement.querySelector('#error-box').innerHTML = 'WRONG NICKNAME! (3-12 characters)';
+    } else {
+      console.log('registerUser() fired: hello ' + this.username);
+      this.ref.nativeElement.querySelector('#login-box').remove();
+      this.ref.nativeElement.querySelector('#message-form > input').focus();
+      this.chatService.initSocket();
+      this.chatService.sendUsername(this.username);
+
+      this.connectionMsgs = this.chatService.getMessages().subscribe(message => {
+        this.messages.push(message);
+        this.updateScroll();
+      });
+      this.connectionUsers = this.chatService.getUsers().subscribe(user => {
+        this.users = user as string[];
+      });
+    }
+  }
+
+  private loginValidation() {
+    if(this.username == '' || this.username.length < 3){
+      return false;
+    }
+    return true;
   }
 
   updateScroll(){
     let element = this.ref.nativeElement.querySelector("#chat-box");
     element.scrollTop = element.scrollHeight;
+  }
+
+  clearMessages(){
+    this.messages = [];
+    this.ref.nativeElement.querySelector('#message-form > input').focus();
   }
 
   ngOnInit() {
